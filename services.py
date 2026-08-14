c01037530d2d9c5319c8a6d673e32d5787aa07fa
Uniquedigitals <85360821+uniquedigitals@users.noreply.github.com>
2026-08-15 00:49:23 +0100

    Update services.py

--- services.py
+++ services.py
@@ -1,11 +1,13 @@
 from datetime import datetime
+from zoneinfo import ZoneInfo
 
 def current_time():
-    now = datetime.now()
-    
+    now = datetime.now(ZoneInfo("Africa/Lagos"))
+
     formatted = now.strftime("%b %d %Y %I:%M%p")
     formatted = formatted[:-2] + formatted[-2:].lower()
-    
+
     return formatted
 
 
+print(current_time())
